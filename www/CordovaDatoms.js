d4eea20299125d5390eae37b9ffcf44dae435b53
Maniteja <maniteja.chitla@datoms.io>
2024-02-23 13:36:07 +0530

    rename versions

--- www/CordovaDatoms.js
+++ www/CordovaDatoms.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cordovaDatoms', 'coolMethod', [arg0]);
+    exec(success, error, 'CordovaDatoms', 'coolMethod', [arg0]);
 };
